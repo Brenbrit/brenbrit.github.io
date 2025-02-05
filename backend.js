7e8fe16c3a6b46d586ed5387369accb7b036550b
Brendan Guillory <brendanguillory@gmail.com>
2025-02-04 19:52:57 -0800

    Adds upload button and labels

--- backend.js
+++ backend.js
@@ -1,7 +1,35 @@
 "use strict";
 
-var wordInput = document.getElementById("word-input");
-
-handleKeyPress = event => {
-    console.log(wordInput.value)
+function downloadClicked() {
+    console.log("Upload clicked!")
+    console.log("Value of text area: " + document.getElementById("inputArea").value)
 }
+
+function displayFileContents(contents) {
+    var outputAreaValue = "Uploaded file bytes: ";
+
+    outputAreaValue += contents.byteLength;
+
+    document.getElementById("outputArea").value = outputAreaValue;
+}
+
+function uploadClicked(e) {
+    // Get the file we'll be reading
+    var file = e.target.files[0];
+    if (!file) {
+        return;
+    }
+
+    // Write down what happens when we read the file
+    var reader = new FileReader();
+    reader.onload = function(e) {
+        var contents = e.target.result;
+        displayFileContents(contents);
+    }
+
+    // Read the file
+    reader.readAsArrayBuffer(file);
+}
+
+// Add listener
+document.getElementById("fileUpload").addEventListener('change', uploadClicked, false)
